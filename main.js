c7917748029fd60cc411b3e1727384e290ffa245
xhbalta <anay.dcho@gmail.com>
2026-03-14 21:22:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // main.js - Router principal con soporte completo para búsqueda e integración con sidebar y reproductor
 import { DATA, renderFeed, renderGrid, renderEpisodio, renderSerie, renderCategoryPills } from './show.js';
 import { getEpisodioByDetailUrl, getSerieByUrl, getAllEpisodios } from './episodios.js';
+import './player.js';
 
 // Páginas especiales
 const PAGES = [
