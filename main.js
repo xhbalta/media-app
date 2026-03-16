0ed9dc2617991c8b7c4147834dbb0e5c73b176d6
xhbalta <anay.dcho@gmail.com>
2026-03-16 09:28:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // main.js - Router principal con soporte completo para SPA (versión corregida)
 import { DATA, renderFeed, renderGrid, renderEpisodio, renderSerie, renderCategoryPills } from './show.js';
-import { getEpisodioByDetailUrl, getSerieByUrl } from 'https://baltaestudiante.github.io/menu/episodios.js';
+import { getEpisodioByDetailUrl, getSerieByUrl } from 'https://podcast.tenam.site/episodios.js';
 import './player.js';
 
 const PAGES = [
