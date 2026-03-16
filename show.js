d6ef7011c372aaecd7a146746db51549ab04d349
xhbalta <anay.dcho@gmail.com>
2026-03-16 09:25:37 -0600

    Update show.js

--- show.js
+++ show.js
@@ -1,5 +1,5 @@
 // show.js - Vistas del feed, episodio, serie, etc. - VERSIÓN COMPLETA CORREGIDA
-import { getAllEpisodios, getSerieById, getEpisodiosBySerieId, getEpisodiosConSerie } from './episodios.js';
+import { getAllEpisodios, getSerieById, getEpisodiosBySerieId, getEpisodiosConSerie } from 'https://baltaestudiante.github.io/menu/episodios.js';
 import { userStorage } from './storage.js';
 import './player.js';
 
