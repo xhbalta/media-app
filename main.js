cdf16edb4bf12eadc2bd29039f8dcffeb12651b9
xhbalta <anay.dcho@gmail.com>
2026-03-18 13:25:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js - Router principal (versión corregida y compatible con tu show.js)
 
 import { DATA, renderFeed, renderGrid, renderEpisodio, renderSerie, renderCategoryPills } from './show.js';
-import { getEpisodioByDetailUrl, getSerieByUrl } from './episodios.js';
+import { getEpisodioByDetailUrl, getSerieByUrl } from 'https://podcast.tenam.site/episodios.js';
 import './player.js';
 
 function updateCanonicalAndAlternate() {
