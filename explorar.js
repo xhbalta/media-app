a54b5d66daa07b63d65447ee09c364b7424c286f
xhbalta <anay.dcho@gmail.com>
2026-03-18 13:25:25 -0600

    Update explorar.js

--- explorar.js
+++ explorar.js
@@ -1,5 +1,5 @@
 // explorar.js - Página de exploración con diseño profesional
-import { getEpisodiosConSerie, getAllEpisodios, series } from './episodios.js';
+import { getEpisodiosConSerie, getAllEpisodios, series } from 'https://podcast.tenam.site/episodios.js';
 
 // ---------- CONSTANTES ----------
 const ICONS = {
